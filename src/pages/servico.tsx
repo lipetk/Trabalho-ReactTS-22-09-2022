ae08ae6b43eefd5f0a47195f169f3e7bd3fef6d0
lipetk <souzadrian728@gmail.com>
2022-09-22 16:24:43 -0300

    renomeado

--- src/pages/servico.tsx
+++ src/pages/servico.tsx
@@ -2,7 +2,7 @@ import React from "react";
 export const Servicos = ()=>{
     return (
         <>
-        <h1 className="pagServicos">Página Serviços asd</h1>
+        <h1 className="pagServicos">Página Serviços</h1>
         <h2></h2>
         </>
     );
